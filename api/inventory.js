daf7bfca0c97e44398f594107ce185b999eda2ec
Vedu0 <153887835+Vedu0@users.noreply.github.com>
2026-05-28 02:22:55 +0530

    added inventory.js

---
+++ api/inventory.js
@@ -0,0 +1,33 @@
+export default function handler(req, res) {
+
+      const inventory = [
+
+          {
+                id: 1,
+                      container: "Container 1",
+                            quantity: 10,
+                                  capacity: 15,
+                                        status: "FULL"
+                                            },
+
+                                                {
+                                                      id: 2,
+                                                            container: "Container 2",
+                                                                  quantity: 5,
+                                                                        capacity: 15,
+                                                                              status: "LOW STOCK"
+                                                                                  },
+
+                                                                                      {
+                                                                                            id: 3,
+                                                                                                  container: "Container 3",
+                                                                                                        quantity: 0,
+                                                                                                              capacity: 15,
+                                                                                                                    status: "EMPTY"
+                                                                                                                        }
+
+                                                                                                                          ];
+
+                                                                                                                            res.status(200).json(inventory);
+
+                                                                                                                            }
