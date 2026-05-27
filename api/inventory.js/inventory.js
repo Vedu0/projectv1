0b1579d8b733da5774ad7f1352cb1d379833835b
Vedu0 <153887835+Vedu0@users.noreply.github.com>
2026-05-28 02:13:10 +0530

    changes

--- api/inventory.js/inventory.js
+++ api/inventory.js/inventory.js
@@ -31,3 +31,10 @@ export default function handler(req, res) {
                                                                                                                             res.status(200).json(inventory);
 
                                                                                                                             }
+export default function handler(req, res) {
+
+        res.status(200).json({
+            message: "API WORKING"
+              });
+
+              }
